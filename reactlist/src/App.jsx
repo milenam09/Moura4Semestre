da9729a1702c2ce89f09ec7fa80b25a9f6dc389b
MILENA OLIVEIRA MARES <47269169802@info.local>
2026-07-31 14:46:00 -0300

    novos

--- reactlist/src/App.jsx
+++ reactlist/src/App.jsx
@@ -1,19 +1,103 @@
+import axios from 'axios';
 import './App.css'
 import penIcon from "./assets/edit.2.svg"
 import trashIcon from "./assets/trash.2.svg"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [tasklist, setTasklist] = useState([
-    { id: 1, description: "Revisar HTML" },
-    { id: 2, description: "Revisar CSS" },
-    { id: 3, description: "Revisar ReactJS" },
-    { id: 4, description: "Aprender React Navigate" }
-  ])
+  const [tasklist, setTasklist] = useState([]);
+  const [taskValue, setTaskValue] = useState("")
+  const [editMode, setEditMode] = useState(false)
+  const [idToEdit, setIdToEdit] = useState(0)
 
+  const getTasks = async () => {
+    try {
+        const APIReturn = await axios.get("http://localhost:3000/taskpoint")
+        const APIData = await APIReturn.data
+        // ATUALIZA O STATE
+        setTasklist(APIData)
+    } catch (error) {
+      console.log(erro);
+    }
+  }
+  //Get
+  const getTaskById = async (id) => {
+    alert(`Função getTasksById em desenvolvimento ${id}`)
+  }
 
+  //Post
+  const postTask = async (e) => {
+    e.preventDefault()
+    if(taskValue.trim().length == 0){
+      alert("Preencher o campo valor")
+      return false
+    }
+    try {
+      const APIReturn = await axios.post("http://localhost:3000/taskpoint", {
+        descricao : taskValue
+      })
+      setTaskValue("")
+      getTasks()
+    } catch (error) {
+      console.log(error);
 
+      alert("Erro ao cadastrar os dados")
+    }
+     
+  }
+
+  //Put Pre-Editar
+  const putTask = (item) => {
+    setEditMode(true)
+    setIdToEdit(item.id)
+    setTaskValue(item.descricao)
+  }
+
+  const confirmPutTask = async (e) => {
+    e.preventDefault()
+
+    if (taskValue.trim().length == 0) {
+      alert("Preencha o texto da tarefa")
+      return false;
+    }
+
+    try {
+      const APIReturn = await axios.put(`http://localhost:3000/taskpoint/${idToEdit}`, {descricao : taskValue})
+      setIdToEdit(0);
+      setTaskValue("");
+      alert("A tarefa foi editada");
+      getTasks();
+    } catch (error) {
+      alert("Erro ao editar")
+      console.log(error);
+    }
+    
+  }
+
+  //Delete
+  const deleteTask = async (id) => {
+
+    const querExcluir = confirm(`Quer realmente excluir o registro ?`)
+    if (!querExcluir){
+       return false;
+    }
+
+     try {
+      const APIReturn = await axios.delete(`http://localhost:3000/taskpoint/${id}`)    
+      getTasks()
+      alert("Tarefa excluida com sucesso!!")
+    } catch (error) {
+      console.log(error);
+
+      alert("Erro ao excluir os dados")
+    }
+  }
+
+  useEffect(() =>{
+    //carrega os dados quando o componente for montado!
+    getTasks()
+  }, [])
 
   return (
     <>
@@ -22,12 +106,35 @@ function App() {
       </header>
 
       <main className="body-section">
-        <form className="cad-task">
+        <form className="cad-task" onSubmit={editMode ? confirmPutTask : postTask }>
           <input className="card-task__entry"
             type="text"
             placeholder='Adicione uma tarefa'
+            value={taskValue}
+            onChange={(e) => [
+              setTaskValue(e.target.value)
+            ]}
           />
+          <p>{taskValue}</p>
+          <p>{editMode ? "true" : "false"}</p>
+
           <button className="card-task__btn-confirm">Adicionar</button>
+
+              {
+                editMode && (
+                <button 
+                  className="card-task__btn-confirm" 
+                  type="button"
+                  onClick={() => {
+                    setTaskValue("")
+                    setIdToEdit(0)
+                    setEditMode(false)
+                  }}
+                  >
+                    Cancelar
+                    </button>
+              )}
+
         </form>
 
         <section className="cardlist">
@@ -35,18 +142,34 @@ function App() {
           {
             tasklist.map((t) => {
               return (
-                <article className="cardtask">
+                <article className="cardtask" key={t.id}>
                   <p className='cardtask__tasc-text'>
-                    {t.description}
+                    {t.descricao}
                     </p>
 
                   <div className="carstask__icon-box">
 
-                    <div className='cardlist__icon'>
-                      <img src={penIcon} className='cardlist__edit-icon' alt="" />
+                    <div className="cardlist__icon">
+                      <img src={penIcon} 
+                      className="cardlist__edit-icon" 
+                      alt="imagem de um lapis.Funcao de editar a tarefa"
+                      onClick={() => {
+                        //variavel t e o item/objeto completo
+                        putTask (t)
+                      }} />
+                      
                     </div>
-                    <div className='cardlist__icon'>
-                      <img src={trashIcon} className='delet-icon' alt="" />
+
+                    <div className="cardlist__icon">
+                    <img 
+                      src={trashIcon}
+                      className="cardlist__delet-icon" 
+                      alt="imagem de uma lixeira.Funcao de editar a tarefa" 
+                      onClick={() => {
+                        deleteTask (t.id)
+                      }}
+
+                    />
                     </div>
                   </div>
 
